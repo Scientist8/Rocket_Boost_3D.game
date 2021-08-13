e56d5e6fc4974aca77481f4affad385023df6082
Can Karaca <cankaraca94@gmail.com>
2021-08-13 11:46:03 +0300

    Added oscillator
    Added an oscillator script which uses a sin wave to oscillate objects, giving player more diverse and dynamic environment.

---
+++ Rocket Boost/Assets/Scripts/Oscillator.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oscillator : MonoBehaviour
+{
+    Vector3 startingPosition;
+    [SerializeField] Vector3 movementVector;
+    float movementFactor;
+    [SerializeField] float period = 2f;
+
+    void Start()
+    {
+        startingPosition = transform.position;
+    }
+
+    void Update()
+    {
+        if (period == 0f) // so the cycles will not be divided by 0
+        {
+            return;
+        }
+
+        float cycles = Time.time / period; // continually growing over time
+
+        const float tau = Mathf.PI * 2; // constant value of 6.283...
+        float rawSinWave = Mathf.Sin(cycles * tau); // Going from -1 to 1
+
+        movementFactor = (rawSinWave + 1f) / 2f; // recalculated to go from 0 to 1 so it's cleaner
+
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startingPosition + offset;
+    }
+}
